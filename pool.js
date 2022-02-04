8f5386b81f6ea4c164ad6709706b79b6f62e5e52
AndrianovaVeronika <89983879+AndrianovaVeronika@users.noreply.github.com>
2022-02-04 20:57:58 +0200

    test

--- pool.js
+++ pool.js
@@ -3,23 +3,23 @@ const { Client, Pool } = require('pg');
 require('dotenv').config();
 const logger = require('./utils/logger');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    url: process.env.DATABASE_URL
-})
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//     url: process.env.DATABASE_URL
+// })
 
-// const client = new Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-//
-// client.connect();
+const pool = new Client({
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+pool.connect();
 
 // pool.query('CREATE TABLE IF NOT EXISTS cities (id integer NOT NULL PRIMARY KEY,name varchar NOT NULL)', (error, result) => {
 //     if (error) {
