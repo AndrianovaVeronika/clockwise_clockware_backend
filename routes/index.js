03ef5087b88342c07125fa509c3e5751916c8053
AndrianovaVeronika <89983879+AndrianovaVeronika@users.noreply.github.com>
2022-02-10 18:48:36 +0200

    delete: outdated files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const mastersRouter = require('./masters');
-const ordersRouter = require('./orders');
-const citiesRouter = require('./cities');
-const express = require("express");
-const router = express.Router();
-
-router.use(mastersRouter, ordersRouter, citiesRouter);
-
-module.exports = router;
