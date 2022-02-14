77219651ef7323332d65e79d5975745b8f15a694
Annika <andrianova.v20@cf.dnu.dp.ua>
2022-02-14 10:40:37 +0200

    fix: middleware test

--- app/models/index.js
+++ app/models/index.js
@@ -24,11 +24,11 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("../models/user.model.js")(sequelize, Sequelize);
-db.role = require("../models/role.model.js")(sequelize, Sequelize);
-db.city = require("../models/city.model")(sequelize, Sequelize);
+db.user = require("./user.model.js")(sequelize, Sequelize);
+db.role = require("./role.model.js")(sequelize, Sequelize);
+db.city = require("./city.model")(sequelize, Sequelize);
 db.clock_type = require("./clockType.model")(sequelize, Sequelize);
-db.master = require("../models/master.model")(sequelize, Sequelize);
+db.master = require("./master.model")(sequelize, Sequelize);
 db.order = require("../models/order.model")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
@@ -53,7 +53,9 @@ db.master.belongsToMany(db.city, {
     otherKey: "city_id"
 });
 
-db.order.belongsTo(db.user);
+db.order.belongsTo(db.user, {
+    foreignKey: "user_id"
+});
 db.user.hasOne(db.order);
 db.order.belongsTo(db.city);
 db.city.hasOne(db.order);
