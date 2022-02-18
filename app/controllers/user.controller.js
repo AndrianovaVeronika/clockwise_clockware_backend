157d263033a8d838239cc70ffb2b56459cb8448d
Annika <andrianova.v20@cf.dnu.dp.ua>
2022-02-18 23:19:09 +0200

    fix: authorization user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -9,10 +9,23 @@ exports.userBoard = (req, res) => {
     const id = req.userId;
     logger.info(`Finding user with id=${id}...`);
     User.findByPk(id)
-        .then(data => {
-            if (data) {
+        .then(user => {
+            if (user) {
                 logger.info('User found');
-                res.status(200).send(data);
+                const authorities = [];
+                user.getRoles().then(roles => {
+                    for (let i = 0; i < roles.length; i++) {
+                        authorities.push("ROLE_" + roles[i].name.toUpperCase());
+                    }
+                    logger.info('Signed in successfully!');
+                    res.status(200).send({
+                        id: user.id,
+                        username: user.username,
+                        email: user.email,
+                        roles: authorities,
+                        password: user.password
+                    });
+                });
             } else {
                 logger.info(`Cannot find user with id=${id}`);
                 res.status(404).send({
