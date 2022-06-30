3711bf38906f85898f02cc249d7a99ee24dc3d17
andrianova.v20 <andrianova.v20@cf.dnu.dp.ua>
2022-06-30 08:33:52 +0200

    feat: unique columns in cities and users

--- models/city.js
+++ models/city.js
@@ -11,7 +11,10 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     City.init({
-        name: DataTypes.STRING
+        name: {
+            type: DataTypes.STRING,
+            unique: true
+        }
     }, {
         sequelize,
         modelName: 'City',
