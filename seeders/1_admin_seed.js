a3f9bccc9b832d60e5cff560c74d2f493cb48418
andrianova.v20 <andrianova.v20@cf.dnu.dp.ua>
2022-09-09 14:20:40 +0200

    fix: admin-seed

--- seeders/1_admin_seed.js
+++ seeders/1_admin_seed.js
@@ -5,7 +5,7 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         return queryInterface.bulkInsert('Users', [
             {
-                username: process.env.INITIAL_ADMIN_USERNAME,
+                name: process.env.INITIAL_ADMIN_USERNAME,
                 password: getBcryptedPassword(process.env.INITIAL_ADMIN_PASSWORD),
                 email: process.env.INITIAL_ADMIN_EMAIL,
                 emailChecked: true,
