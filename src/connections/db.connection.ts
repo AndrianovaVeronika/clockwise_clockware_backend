a709605ed0d33662652d99e5700b71fa3cadfd1c
andrianova.v20 <andrianova.v20@cf.dnu.dp.ua>
2022-09-22 14:02:10 +0200

    feat: connection

--- src/connections/db.connection.ts
+++ src/connections/db.connection.ts
@@ -1,6 +1,7 @@
 import {Sequelize} from "sequelize";
 import config from "../config/db.config";
-const dbConfig = config[process.env.NODE_ENV];
+
+const dbConfig = config[process.env.NODE_ENV as keyof typeof config];
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
 
