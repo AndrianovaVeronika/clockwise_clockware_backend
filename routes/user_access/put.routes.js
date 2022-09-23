90673c6556812d14faefbb73748df01fe8ff5c0d
andrianova.v20 <andrianova.v20@cf.dnu.dp.ua>
2022-09-23 02:39:37 +0200

    feat: validatePasswordChange.js fix: check new password with previous one

--- routes/user_access/put.routes.js
+++ routes/user_access/put.routes.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const userController = require("../../controllers/user.controller");
 const orderController = require("../../controllers/order.controller");
 const {verifyTokenAndExtractUserId, ifBodyUndefined} = require("../../validators");
+const validatePasswordChange = require("../../validators/validatePasswordChange");
 
 router.use(ifBodyUndefined);
 router.use(verifyTokenAndExtractUserId);
 
-router.put("/update/credentials", userController.update);
+router.put("/update/credentials", [validatePasswordChange], userController.update);
 
 router.put("/rate/order/:id", orderController.rateOrder);
 
